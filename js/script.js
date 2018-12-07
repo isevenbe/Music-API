2ff1738bb474766f4e00795d4428a11aaff6de7a
Florentin Lefort <florentin.lefort@gmail.com>
2018-12-07 14:10:39 +0100

    Add files via upload
    Refactoring Code

--- js/script.js
+++ js/script.js
@@ -11,42 +11,53 @@
     return document.querySelector(element)
   }
 
-  const search = value => {
+  const createListOfArtist = data => {
     const selectMyList = selectHTML(".results");
+    data.map(function (artist) {
+      let li = createNode("li");
+      let a = createNode("button");
+      a.innerHTML = `${artist.name}`;
+      a.id = `${artist.id}`;
+      appendNode(li, a);
+      appendNode(selectMyList, li);
+
+      selectArtist = document.getElementById(`${artist.id}`);
+      selectArtist.addEventListener("click", () => {
+        selectHTML(".artist").innerHTML = artist.name;
+        urlID = `https://musicdemons.com/api/v1/artist/${artist.id}/songs`
+        createListOfVideo();
+      });
+    })
+  }
+
+  const createListOfVideo = () => {
+    fetch(urlID).then((resp) => resp.json()).then(function (data) {
+      data.map(function (ID) {
+        let songSelect = selectHTML(".songs");
+        let liSong = createNode("li");
+        let iframe = createNode("iframe");
+        iframe.setAttribute('min-width', '600');
+        iframe.setAttribute('min-height', '400');
+        iframe.setAttribute("src", `https://www.youtube.com/embed/${ID.youtube_id}`);
+        appendNode(liSong, iframe);
+        appendNode(songSelect, liSong);
+      })
+
+    })
+
+  }
+
+
+
+  const search = value => {
     let url = `https://musicdemons.com/api/v1/artist/autocomplete`;
     fetch(url, {
       method: "POST",
       body: JSON.stringify(value)
-    }).then((resp) => resp.json()).then(function (data) {
-      return data.map(function (artist) {
-        let li = createNode("li");
-        let a = createNode("button");
-        a.innerHTML = `${artist.name}`;
-        a.id = `${artist.id}`;
-        appendNode(li, a);
-        appendNode(selectMyList, li);
-
-        let selectArtist = document.getElementById(`${artist.id}`);
-        selectArtist.addEventListener("click", function () {
-          selectHTML(".artist").innerHTML = artist.name;
-          const urlID = `https://musicdemons.com/api/v1/artist/${artist.id}/songs`
-          fetch(urlID).then((resp) => resp.json()).then(function (data) {
-            data.map(function (ID) {
-              let songSelect = selectHTML(".songs");
-              let liSong = createNode("li");
-              let iframe = createNode("iframe");
-              iframe.setAttribute('min-width', '600');
-              iframe.setAttribute('min-height', '400');
-              iframe.setAttribute("src", `https://www.youtube.com/embed/${ID.youtube_id}`);
-              appendNode(liSong, iframe);
-              appendNode(songSelect, liSong);
-            })
-
-          })
-        })
-      });
     })
 
+      .then((resp) => resp.json())
+      .then(dataResult => createListOfArtist(dataResult))
   };
 
 
@@ -54,8 +65,6 @@
   const handleKeyUpSearch = e => {
     const $input = e.currentTarget;
     search($input.value);
-
-
   };
 
   const init = () => {
